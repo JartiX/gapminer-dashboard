e6252f1fcf3b4a901cc57cabf0ce041e00798433
JartiX <artempilavin@gmail.com>
2025-03-31 01:48:52 +0800

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,7 @@
 from dashboard import application
+import os
+
 
 if __name__ == "__main__":
-    application.run()
+    port = int(os.environ.get("PORT", 10000))
+    application.run(host='0.0.0.0', port=port)
